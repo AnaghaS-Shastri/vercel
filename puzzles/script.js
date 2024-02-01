bc64089042e382cc52691f686d5dc99f5bd958e7
AnaghaS-Shastri <149950166+AnaghaS-Shastri@users.noreply.github.com>
2024-02-01 20:12:46 +0530

    Add files via upload

---
+++ puzzles/script.js
@@ -0,0 +1,11 @@
+// Sudoku Puzzle
+function generateSudoku() {
+    // Implement Sudoku generation logic here
+    alert("Generate Sudoku - Coming Soon!");
+}
+
+// Crossword Puzzle
+function generateCrossword() {
+    // Implement Crossword generation logic here
+    alert("Generate Crossword - Coming Soon!");
+}
